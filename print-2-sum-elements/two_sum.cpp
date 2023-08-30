277ba92b43948c0b315d5ac8bd3f18ee55aa13ed
Nakkina veera venkatasomasekhar <134932400+nakkina-sekhar@users.noreply.github.com>
2023-08-30 17:50:50 +0530

    Update two_sum.cpp

--- print-2-sum-elements/two_sum.cpp
+++ print-2-sum-elements/two_sum.cpp
@@ -1,8 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
+#include <bits/stdc++.h>    //test case:1              test case:2                 
+using namespace std;        //A[]=[3,6,1,2,8] sum=14   A[]=[3,6,8,10,15,9]  sum=19
+int main()                   //out: 6 8                out: 9 10
+{  //time complexity:o(n^2)
     int n, sum;
     cin >> n;
 
@@ -11,7 +10,7 @@ int main()
         cin >> a[i];
     }
     cin >> sum;
-    sort(a,a+n);
+    sort(a,a+n);  //this function return sorted array
     int l=0, h=n-1;  //two pointer approach
    while(l<h){
      if(a[l]+a[h]==sum){
